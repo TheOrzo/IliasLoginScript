2554d854a767480d1f74ed5b2606a3b091bc1e57
Orzo <45884932+TheOrzo@users.noreply.github.com>
2022-05-18 10:32:33 +0200

    Create KitLoginRedirect.js

---
+++ KitLoginRedirect.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://idp.scc.kit.edu/idp/profile/SAML2/Redirect/SSO?execution=e1s1
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.getElementById("sbmt").click();
+})();
